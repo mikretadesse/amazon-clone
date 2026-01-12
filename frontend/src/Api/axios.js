c5f99aa70807da62c6088618b31ea87e4271c0a0
mikretadesse <mikretadesse09@gmail.com>
2026-01-12 17:45:16 +0300

    deployment

--- frontend/src/Api/axios.js
+++ frontend/src/Api/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-e4553/us-central1/api",
+  // 1. Local development backend (Firebase Functions Emulator)
+  // baseURL: "http://127.0.0.1:5001/clone-e4553/us-central1/api",
+
+  // 2. Use this URL when Amazon-clone backend is deployed on Render.com
+  baseURL: "https://mikre-amazon-clone-2025-backend.onrender.com",
 });
 
 export { axiosInstance };
